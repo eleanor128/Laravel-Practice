2e97f984ab5873b7ea720356084fde977b49831e
Eleanor <eleanor128@gmail.com>
2021-11-26 17:40:58 +0800

    first commit

---
+++ resources/views/articles/list.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('title')
+    {{ __('List OF Articles') }}
+@endsection
+
+@section('header')
+
+@endsection
+
+@section('content')
+    <h1>This page has a lot of articles</h1>
+@endsection
