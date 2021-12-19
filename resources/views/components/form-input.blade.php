d13f245855d819a66df3a3b563d7504aac8d6ed9
Eleanor <eleanor128@gmail.com>
2021-12-20 02:36:50 +0800

    12/20 do some components

---
+++ resources/views/components/form-input.blade.php
@@ -0,0 +1,2 @@
+<input type="{{ $type }}" name="{{ $name }}"
+    {{ $attributes->class(['bg-gray-50 border text-gray-900 sm:text-sm rounded-lg block w-full p-2.5', 'border-gray-300 focus:ring-blue-500 focus:border-blue-500' => !$errors->has($name), 'border-red-600 focus:ring-red-500 focus:border-red-500' => $errors->has($name)]) }}>
